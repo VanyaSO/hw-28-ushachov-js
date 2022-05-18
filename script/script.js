4e3f6bac3b7d5a70fbcdd63a6ec33c6b0bb3df57
Ivan Ushachov <90464856+VanyaSO@users.noreply.github.com>
2022-05-18 20:12:16 +0300

    ?

--- script/script.js
+++ script/script.js
@@ -1,37 +1,41 @@
 'use strict';
 
-let num1;
-let num2;
+// let num1;
+// let num2;
 
 
 let calculator = {
-    read: function read () {
-        num1 = +prompt('Введите число 1');
-        num2 = +prompt('Введите число 2');
+    num1: 0,
+    num2: 0,
 
-        if( typeof num1 !== 'number' ){
-            num1 = 0;
-        }else if(typeof num2 !== 'number'){
-            num2 = 0;
+    read: function () {
+        this.num1 = +prompt('Введите число 1');
+        this.num2 = +prompt('Введите число 2');
+
+        if( typeof this.num1 !== 'number' ){
+            this.num1 = 0;
+        }else if(typeof this.num2 !== 'number'){
+            this.num2 = 0;
         }
 
-        return{
-            num1,
-            num2
-        }
     },
 
-    sum: function (num1, num2) {
-        return num1 + num2;
+    sum: function () {
+        console.log(this.num1 + this.num2);
     },
 
-    mul: function mul (num1, num2) {
-        return num1 * num2;
+    mul: function () {
+        console.log(this.num1 * this.num1);
     }
 
 
 };
 
-console.log(calculator.read());
-console.log(calculator.sum(num1, num2));
-console.log(calculator.mul(num1, num2));
+console.log(calculator);
+
+calculator.read();
+calculator.sum();
+calculator.mul();
+
+// console.log(calculator.sum(num1, num2));
+// console.log(calculator.mul(num1, num2));
